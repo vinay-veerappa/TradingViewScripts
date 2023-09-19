644deb760a29905590893d2c3427ab6e72931b8f
vinay-veerappa <vinay.veerappa@gmail.com>
2023-09-19 14:30:32 -0700

    Create BarUpDown.cs
    Bar Up/Down + HMA(80) Filter.
    Needs HeikenAishi candles.

---
+++ BarUpDown.cs
@@ -0,0 +1,329 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class BarUpDown : Strategy
+	{
+		private int		breakEvenTicks		= 20;		// Default setting for ticks needed to acheive before stop moves to breakeven		
+		private int		plusBreakEven		= 10; 		// Default setting for amount of ticks past breakeven to actually breakeven
+		private int 	BarTraded 			= 0; 		// Default setting for Bar number that trade occurs	
+		private double	initialBreakEven	= 0; 		// Default setting for where you set the breakeven
+		private double 	previousPrice		= 0;		// previous price used to calculate trailing stop
+		private double 	newPrice			= 0;		// Default setting for new price used to calculate trailing stop
+		private HMA hmaHigh;
+		
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Tries to ride a trend using HA candles with a confirmation of 2/3 candles";
+				Name										= "BarUpDown";
+				Calculate									= Calculate.OnBarClose;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 60;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+				LongTermMAPeriod					= 81;
+				Use3Candles					= true;
+				EnableTimeOfDay					= true;
+				Startime						= DateTime.Parse("23:00", System.Globalization.CultureInfo.InvariantCulture);
+				EndTime						= DateTime.Parse("13:00", System.Globalization.CultureInfo.InvariantCulture);
+
+				UseATRforStopLoss					= false;
+	
+				
+				//breakEvenTicks		= 4;		// Default setting for ticks needed to acheive before stop moves to breakeven		
+				//plusBreakEven		= 2; 		// Default setting for amount of ticks past breakeven to actually breakeven
+				profitTargetTicks	= 80;		// Default setting for how many Ticks away from AvgPrice is profit target
+		        stopLossTicks		= 40;		// Default setting for stoploss. Ticks away from AvgPrice		
+				trailProfitTrigger	= 20;		// 8 Default Setting for trail trigger ie the number of ticks movede after break even befor activating TrailStep
+				trailStepTicks		= 8;		// 2 Default setting for number of ticks advanced in the trails - take into consideration the barsize as is calculated/advanced next bar
+			}
+				
+			else if (State == State.Configure)
+			{
+				//AddHeikenAshi(BarsPeriodType.Minute,2);
+				SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+				SetProfitTarget(CalculationMode.Ticks, profitTargetTicks);
+			}
+			else if (State == State.DataLoaded)
+			{
+				
+				// Add simple moving averages to the chart for display
+				// This only displays the SMA's for the primary Bars object on the chart
+				// Note only indicators based on the charts primary data series can be added.
+				hmaHigh = HMA(LongTermMAPeriod);
+				//hmaLow = HMA(50);
+				AddChartIndicator(hmaHigh);
+			}
+		}
+		
+		private void FillLongEntry1()
+		{
+			Print("Entering Long");
+			EnterLong(Convert.ToInt32(DefaultQuantity), "");
+			BarTraded = CurrentBar;
+		}
+		
+		private void FillShortEntry1()
+		{
+			Print("Entering Short");	
+			EnterShort(Convert.ToInt32(DefaultQuantity), "");
+			BarTraded = CurrentBar;
+		}  
+		
+		protected override void OnBarUpdate()
+		{
+			if (BarsInProgress != 0) 
+				return;
+			// Can restrict trading on Sundays and partial Holidays and Holiday
+			// Time zone used in current PC time zone. Should convert to New York time and use 
+			
+			bool validTimeforTrading = false;
+			
+			// Exit all positions at 12:55 PST
+			if (ToTime(Time[0]) >= 130000)
+			{
+				if (Position.MarketPosition == MarketPosition.Long)
+					ExitLong();
+				if (Position.MarketPosition == MarketPosition.Short)
+					ExitShort();
+			}
+			
+			if(ToTime(Time[0]) <= 200000 && ToTime(Time[0]) >= 125000)
+			{
+				// Outside Trading hours
+				//return;
+			}
+
+			if (CurrentBars[0] < 2)
+				return;
+
+			 //LONG CONDITION
+			// Check Three Bars 
+			// can also check for displacement etc??? sometimes that is good for exits
+			if ( (Close[0] > Open[0])
+				&& (Close[0] > Close[1])
+				&& (Close[0] > Close[2])
+				//&& (Close[1] > Close[3])
+				&& IsRising(HMA(LongTermMAPeriod))
+				)
+			{
+				if (Position.MarketPosition == MarketPosition.Short) ExitShort();
+					
+				//longCondition = true ;
+				Draw.ArrowUp(this,"Long Entry"+Low[0].ToString(), true, 0,Low[0]-TickSize, Brushes.Gold);
+					//EnterLongStopLimit(EntriesPerDirection,High[0] + 2 * TickSize, High[0]- 10* TickSize, "longCondition");
+					//EnterLong();
+				FillLongEntry1();
+					//SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+				SetProfitTarget(CalculationMode.Ticks,profitTargetTicks);
+				SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+			}
+			
+			 // SHORT CONDITION
+			if ((Open[0] > Close[0])
+				 && (Open[0] > Open[1]) 
+				 && (Open[0] > Open[2])
+				// && (Open[1] > Open[2])
+				 //&& (Open[0] < Close[3])
+				 && IsFalling(HMA(LongTermMAPeriod))
+				)
+			{
+				if (Position.MarketPosition == MarketPosition.Long) ExitLong();
+					
+				//longCondition = true ;
+				Draw.ArrowDown(this,"Short Entry"+Low[0].ToString(), true, 0,High[0] + TickSize, Brushes.Blue);
+
+				FillShortEntry1();
+					//SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+				SetProfitTarget(CalculationMode.Ticks,profitTargetTicks);
+				SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+			}
+			// Resets the stop loss to the original value when all positions are closed
+			switch (Position.MarketPosition)
+            {
+                case MarketPosition.Flat:
+                    SetStopLoss(CalculationMode.Ticks, stopLossTicks);
+					previousPrice = 0;
+                    break;
+				 case MarketPosition.Long:
+                    // Once the price is greater than entry price+ breakEvenTicks ticks, set stop loss to breakeven
+                    if (Close[0] > Position.AveragePrice + breakEvenTicks * TickSize
+						&& previousPrice == 0 )
+                    {
+						initialBreakEven = Position.AveragePrice + plusBreakEven * TickSize;
+                        //SetStopLoss(CalculationMode.Price, initialBreakEven);
+						previousPrice = Position.AveragePrice;
+						Print("previousPrice = "+previousPrice);
+                    }
+					// Once at breakeven wait till trailProfitTrigger is reached before advancing stoploss by trailStepTicks size step
+					else if (previousPrice	!= 0 ////StopLoss is at breakeven
+ 							&& GetCurrentAsk() > previousPrice + trailProfitTrigger * TickSize
+						)
+					{
+						newPrice = previousPrice + trailStepTicks * TickSize;
+						//SetStopLoss(CalculationMode.Price, newPrice);
+						previousPrice = newPrice;
+						Print("previousPrice = "+previousPrice);
+					}
+					
+					// If the channel is rising then raise the profit target higher
+					if(IsRising( HMA(LongTermMAPeriod)))
+					{
+						//SetProfitTarget(CalculationMode.Price,  trailProfitTrigger * TickSize );
+						
+					}
+					//Long entry will close when price (Close) goes over HF
+					if(IsFalling( HMA(LongTermMAPeriod)))
+					{
+						//closeLongCondition = true ;
+						Draw.Diamond(this,"closeLongCondition"+Low[0].ToString(), true, 0,High[0] + TickSize, Brushes.DeepPink);
+						ExitLong();
+					}
+					//Stop Loss will happen when price (Close) goes under LL
+					
+                    break;
+                case MarketPosition.Short:
+					
+					// If the channel is faling then raise the profit target higher
+					if( IsFalling( HMA(LongTermMAPeriod))
+					  )
+					{
+						//SetProfitTarget(CalculationMode.Price,  trailProfitTrigger * TickSize );
+						
+					}
+                    // Once the price is Less than entry price - breakEvenTicks ticks, set stop loss to breakeven
+                    if (Close[0] < Position.AveragePrice - breakEvenTicks * TickSize
+						&& previousPrice == 0)
+                    {
+						initialBreakEven = Position.AveragePrice - plusBreakEven * TickSize;
+                        //SetStopLoss(CalculationMode.Price, initialBreakEven);
+						previousPrice = Position.AveragePrice;
+						Print("previousPrice = "+previousPrice);
+                    }
+					// Once at breakeven wait till trailProfitTrigger is reached before advancing stoploss by trailStepTicks size step
+					else if (previousPrice	!= 0 ////StopLoss is at breakeven
+ 							&& GetCurrentAsk() < previousPrice - trailProfitTrigger * TickSize
+						)
+					{
+						newPrice = previousPrice - trailStepTicks * TickSize;
+						//SetStopLoss(CalculationMode.Price, newPrice);
+						previousPrice = newPrice;
+						Print("previousPrice = "+previousPrice);
+					}
+					
+					//Short entry will close when price (Close) goes over LF
+					if(IsRising( HMA(LongTermMAPeriod))
+					    )
+					{
+						//closeShortCondition = true ;
+						Draw.Diamond(this,"CloseShortEntry"+Low[0].ToString(), true, 0,Low[0] - TickSize, Brushes.Green);
+						ExitShort();
+					}
+				
+				
+                    break;
+                default:
+					//autoStopA900Traded = false;
+                    break;
+			}
+		}
+
+		#region Properties
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="LongTermMAPeriod", Description="Long term MA for trend detection", Order=1, GroupName="Parameters")]
+		public int LongTermMAPeriod
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name="Use3Candles", Description="Use 2 or 3 candles for confirmation", Order=2, GroupName="Parameters")]
+		public bool Use3Candles
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name="EnableTimeOfDay", Description="Restrict trading during certain times", Order=3, GroupName="Parameters")]
+		public bool EnableTimeOfDay
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[PropertyEditor("NinjaTrader.Gui.Tools.TimeEditorKey")]
+		[Display(Name="Startime", Description="Start time for executions", Order=4, GroupName="Parameters")]
+		public DateTime Startime
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[PropertyEditor("NinjaTrader.Gui.Tools.TimeEditorKey")]
+		[Display(Name="EndTime", Description="End time for trades", Order=5, GroupName="Parameters")]
+		public DateTime EndTime
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="TakeProfitTicks", Description="What is the min Profit target", Order=6, GroupName="Parameters")]
+		public int profitTargetTicks
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="StopLossTicks", Description="What is the StopLoss", Order=7, GroupName="Parameters")]
+		public int stopLossTicks
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name="UseATRforStopLoss", Description="Use ATR instead of ticks for stop Loss", Order=8, GroupName="Parameters")]
+		public bool UseATRforStopLoss
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="TrailProfitTrigger", Description="Profit Trigger for stop loss trailing", Order=9, GroupName="Parameters")]
+		public int trailProfitTrigger
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="TrailStepTicks", Description="Steps at which we will trail the profit", Order=10, GroupName="Parameters")]
+		public int trailStepTicks
+		{ get; set; }
+		#endregion
+
+	}
+}
